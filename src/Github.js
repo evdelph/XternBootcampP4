448db50fd8b0ab6fce0befb7e98d9e88639c8201
Evelyn Marie Delph <evdelph@indiana.edu>
2018-05-29 11:52:51 -0400

    Make fetch work

--- src/Github.js
+++ src/Github.js
@@ -1,46 +1,49 @@
-import React, {Component} from 'react'
-import {Route} from 'react-router-dom'
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 
 import './Github.css'
+import GithubUser from './GithubUser'
 
-class Github extends Component
-{
-
-state = {
+class Github extends Component {
+  state = {
     username: '',
-}
+  }
 
-handleChange = (ev) => {
-    this.setState({username: ev.target.value})
-}
+  handleChange = (ev) => {
+    this.setState({ username: ev.target.value })
+  }
 
-handleSubmit = (ev) => {
+  handleSubmit = (ev) => {
     ev.preventDefault()
     this.props.history.push(`/github/${this.state.username}`)
-    this.setState({username: ''})
-}
+    this.setState({ username: '' })
+  }
 
-render(){
-    return(
-        <div className="Github">
-            <img
-                src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"
-                alt="GitHub"
+  render() {
+    return (
+      <div className="Github">
+        <img
+          className="logo"
+          src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"
+          alt="GitHub"
+        />
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <input
+              type="text"
+              value={this.state.username}
+              onChange={this.handleChange}
             />
-            <form onSubmit={this.handleSubmit}>
-                <div>
-                    <input type="text" value={this.state.username} onChange={this.handleChange}/>
-                </div>
-                <div>
-                    <button type="submit">Look up Github User
-                    </button>
-               </div>
-            </form>
-            <Route path="/github/:username" render={(props) => <h3>You searched for{props.match.params.username}</h3>}/>
-            <Route exact path="/github" render={() => <h3>Please enter a github username</h3>}/>
-        </div>
+          </div>
+          <div>
+            <button type="submit">Look up GitHub user</button>
+          </div>
+        </form>
+        <Route path="/github/:username" component={GithubUser} />
+        <Route exact path="/github" render={() => <h3>Please enter a username to search on GitHub.</h3>} />
+      </div>
     )
-   }
+  }
 }
 
 export default Github
