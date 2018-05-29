801ecf0bca554cd6d663e56f1f89cf3bc8be464d
Evelyn Marie Delph <evdelph@indiana.edu>
2018-05-29 11:09:37 -0400

    Add submit handler to add github user to URL

--- src/Github.js
+++ src/Github.js
@@ -13,6 +13,12 @@ handleChange = (ev) => {
     this.setState({username: ev.target.value})
 }
 
+handleSubmit = (ev) => {
+    ev.preventDefault()
+    this.props.history.push(`/github/${this.state.username}`)
+    this.setState({username: ''})
+}
+
 render(){
     return(
         <div className="Github">
@@ -20,7 +26,7 @@ render(){
                 src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"
                 alt="GitHub"
             />
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <div>
                     <input type="text" value={this.state.username} onChange={this.handleChange}/>
                 </div>
